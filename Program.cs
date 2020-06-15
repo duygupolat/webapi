5332ca751c4dff40875b18c81eeace79bdd6d626
duygupolat <duygupolat@users.noreply.github.com>
2020-06-15 16:15:04 +0300

    First Port

--- Program.cs
+++ Program.cs
@@ -21,6 +21,16 @@ namespace AspNetCoreWebAPI
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
+                    webBuilder.UseKestrel(opts =>
+                {
+                    // Bind directly to a socket handle or Unix socket
+                    // opts.ListenHandle(123554);
+                    // opts.ListenUnixSocket("/tmp/kestrel-test.sock");
+                    //opts.Listen(IPAddress.Loopback, port: 5002);
+                    opts.ListenAnyIP(5000);
+                    opts.ListenLocalhost(5004, opts => opts.UseHttps());
+                    opts.ListenLocalhost(5005, opts => opts.UseHttps());
+                });
                 });
     }
 }
